e6771e10d735fac1919f9f5f2c58f2187deb6844
stevemats <30528167+stevemats@users.noreply.github.com>
2018-10-21 17:51:52 +0300

    Update vein.py

--- vein.py
+++ vein.py
@@ -1,12 +1,14 @@
-#well well well another season new stuffs! |
-#hollards delete! delete! slogan will be replaced today by:
-#findall! extractall! findall! extractall!
+
 import re
 
-pattern = r"([\w\.-]+)@([\w\.-]+)(\.[\w\.]+)"
-str = "Tom and jerry logged in as john@gmail.com at the site which they used to meet bout nuclear energy "
+pattern = r'[\w\.-]+@[\w\.-]+'
+str = '''Tom and jerry logged in as john@gmail.com at the site which they used to meet bout nuclear energy
+        collect all the emails and present them before 2300 military hrs. Send the results to energy@mail.com'''
 
-match = re.search(pattern, str)
+match = re.findall(pattern, str)
 #use findall where there's need to extract more than one email
 if match:
-    print(match.group())
+    print(match)
+
+print ("Emails successfully extracted. Goodbye!")
+
