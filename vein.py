94c151e6ee002b45dbefad57a9b165dbd2a11766
stevemats <30528167+stevemats@users.noreply.github.com>
2018-10-21 22:02:56 +0300

    Update vein.py

--- vein.py
+++ vein.py
@@ -1,14 +1,15 @@
-
+#!env/bin/python
 import re
+import sys
 
 pattern = r'[\w\.-]+@[\w\.-]+'
-str = '''Tom and jerry logged in as john@gmail.com at the site which they used to meet bout nuclear energy
-        collect all the emails and present them before 2300 military hrs. Send the results to energy@mail.com'''
+f = open('secret/samp.txt', 'r')
 
-match = re.findall(pattern, str)
-#use findall where there's need to extract more than one email
+match = re.findall(pattern, f.read())
+
 if match:
     print(match)
 
+print("\n")    
 print ("Emails successfully extracted. Goodbye!")
 
