ddb9ae4269404b3c1bdd5e87c474d95b6b07fad5
stevemats <30528167+stevemats@users.noreply.github.com>
2018-10-28 23:59:42 +0300

    Update vein.py

--- vein.py
+++ vein.py
@@ -16,14 +16,17 @@ print('''
  jgs   `---......____...---`.___.'----... .'         `.;
                                         `-`           `  
 ''')
+print("\n",  "Emails have been successful extracted, checkout the output in the program folder", "\n")
 
 pattern = r'[\w\.-]+@[\w\.-]+'
 f = open('data/info.txt', 'r')
 
 match = re.findall(pattern, f.read())
 
+sys.stdout=open("match.txt","w")
 if match:
     print(match, "\n","\n","Emails successfully extracted. Goodbye!", "\n")
 else:
     print("\n", "No extractable emails found in the document", "\n")
-
+    
+sys.stdout.close()    
