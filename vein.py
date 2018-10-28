c238edb8027bd1382459b15d01419c6f7378ac66
stevemats <30528167+stevemats@users.noreply.github.com>
2018-10-28 22:15:03 +0300

    Update vein.py
    added an else statement to prevent a blank output in case a provided doc doesn't have email(s)

--- vein.py
+++ vein.py
@@ -2,6 +2,21 @@
 import re
 import sys
 
+print('''
+
+
+                                            __
+      o                 /' ) 
+                      /'   (                          ,
+                  __/'     )                        .' `;
+   o      _.-~~~~'          ``---..__             .'   ;
+     _.--'  b)                       ``--...____.'   .'
+    (     _.      )).      `-._                     <
+     `\|\|\|\|)-.....___.-     `-.         __...--'-.'.
+ jgs   `---......____...---`.___.'----... .'         `.;
+                                        `-`           `  
+''')
+
 pattern = r'[\w\.-]+@[\w\.-]+'
 f = open('data/info.txt', 'r')
 
@@ -11,5 +26,7 @@ if match:
     print(match)
 
 print("\n")    
-print ("Emails successfully extracted. Goodbye!")
+print ("Emails successfully extracted. Goodbye!", "\n")
+else:
+    print("\n", "No extractable emails found in the document", "\n")
 
