b6e783895e1083591435e9c85af78e21119dabe2
stevemats <30528167+stevemats@users.noreply.github.com>
2018-11-14 14:03:29 +0300

    Update vein.py
    removed unnecessary comments

--- vein.py
+++ vein.py
@@ -19,13 +19,11 @@ print('''
                                         `-`           `  
 ''')
 
-# pattern to use to find valid emails and path to user data
-# introduced and defined as user input
-
+# regular expression to define the search pattern
 pattern = r'[\w\.-]+@[\w\.-]+\.\w+'
 user_input = input(">>>Enter the path to your file: ");
 
-# //playing around with the time module to customize display method and time
+# user screen starts with a slow printing output of 8 seconds
 def slowprint(videntta):
     for n in videntta + '\n' :
         sys.stdout.write(n)
@@ -34,21 +32,15 @@ def slowprint(videntta):
 slowprint("Starting email extraction... ")
 time.sleep(5)
 
-'''
-exception when the input path is invalid defining the file to read 
-from the user input and a print success containing a list to print
-on a random upon a successful extraction of emails
-'''
+# raising user error message if path not valid
 assert os.path.exists(user_input), "No file to extract emails was found at, "+str(user_input)
 f = open(user_input, 'r')
 print("\n", r.choice(list(open('leaflets/spas.txt'))),"\n")
 
 match = re.findall(pattern, f.read())
 
-'''
-printing out the extracted emails to our desired destination document
-and introducing clauses for our successful/unseccessful extractions
-'''
+
+# Outputs identified emails(results) in the same program folder with a name match.txt
 sys.stdout=open("match.txt","w")
 if match:
     print(match, "\n","\n","Emails successfully extracted. Goodbye!", "\n")
