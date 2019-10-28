93be3d1528a148b89b0a39a7c2b5cd660e7d530e
stevemats <stevematindi@gmail.com>
2019-10-28 04:12:08 +0300

    menu and output formatting
    added the following:
    1. Menu. -> So that a user can chose what he/she wants.( exit or extract)
    2. Restricted users from inputting out of context information in the menu.
    2. Output Formatting. -> Not the best implementation but now lists output in a formatted formatted output as per earlier issue.

--- vein.py
+++ vein.py
@@ -2,12 +2,11 @@
 from __future__ import print_function
 import re
 import time
-import sys, os
+import sys
+import os
 import random as r
 
 print('''
-
-
                                             __
       o                 /' ) 
                       /'   (                          ,
@@ -20,32 +19,61 @@ print('''
                                         `-`           `  
 ''')
 
-# regular expression to define the search pattern
-pattern = r'[\w\.-]+@[\w\.-]+\.\w+'
-user_input = input(">>>Enter the path to your file: ");
 
-# user screen starts with a slow printing output of 8 seconds
-def slowprint(videntta):
-    for n in videntta + '\n' :
-        sys.stdout.write(n)
-        sys.stdout.flush()
-        time.sleep(8. / 100)
-slowprint("Starting email extraction... ")
-time.sleep(5)
+# pattern to use to find valid emails and path to user data
+def main():
+    print('\n1. Extracting emails from document')
+    print('2. Exit')
+    while True:
+        try:
+            choice = int(input('Enter choice: '))
+            if choice == 1:
+                extractor()
+                break
+            elif choice == 2:
+                break
+            else:
+                print("Invalid choice. Enter 1 or 2")
+                main()
+        except ValueError:
+            print("Invalid choice. Enter 1 or 2")
+    exit()
 
-# raising user error message if path not valid
-assert os.path.exists(user_input), "No file to extract emails was found at, "+str(user_input)
-f = open(user_input, 'r')
-print("\n", r.choice(list(open('leaflets/spas.txt'))),"\n")
 
-match = re.findall(pattern, f.read())
+def extractor():
+    # regular expression to define the mail search pattern
+    pattern = r'[\w\.-]+@[\w\.-]+\.\w+'
+    user_input = input(">>>Enter the path to your file: ")
+
+    # user screen starts with a slow printing output of 8 seconds
+    def slowprint(drake):
+        for n in drake + '\n':
+            sys.stdout.write(n)
+            sys.stdout.flush()
+            time.sleep(8. / 100)
+
+    slowprint("Starting email extraction... ")
+    time.sleep(5)
+
+    # raising user error message if path not valid
+    assert os.path.exists(user_input), "No file to extract emails was found at, " + str(user_input)
+    f = open(user_input, 'r')
+    print("\n", r.choice(list(open('leaflets/spas.txt'))), "\n")
+
+    match = re.findall(pattern, f.read())
+
+    for elem in match:
+        # Outputs identified emails(results) in the same program folder with a name match.txt
+        sys.stdout = open("match.txt", "w")
+        # sorting the output in a formatted list
+        if match:
+            print(*match, sep='\n')
+            print("\n", "\n", "Emails successfully extracted. Goodbye!", "\n")
+        else:
+            print("\n", "No extractable emails found in the document", "\n")
+ 
+        sys.stdout.close()
 
 
-# Outputs identified emails(results) in the same program folder with a name match.txt
-sys.stdout=open("match.txt","w")
-if match:
-    print(match, "\n","\n","Emails successfully extracted. Goodbye!", "\n")
-else:
-    print("\n", "No extractable emails found in the document", "\n")
-    
-sys.stdout.close()    
+if __name__ == '__main__':
+    main()
